00509b66d6eced7ddfc91e60ee2b42c481db477e
Yash0494 <62734519+Yash0494@users.noreply.github.com>
2020-03-28 18:16:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[]args){
 
-  System.out.println("Welcome to this phenomenal world");
+  System.out.println("Made some changes in github directly to avoid pusing of code");
   
 }
 }
